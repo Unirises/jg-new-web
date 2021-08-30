b5c0b8e83c08157ed6865510658a3831c0e5e140
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-08-30 13:50:57 +0800

    initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> © JG Express
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Designed & Developed by Unirises, with 💙
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
